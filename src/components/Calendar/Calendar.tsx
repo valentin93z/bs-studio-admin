fc96f584cf30c8ee6e178f95d56b111aeb3d2d0a
valentin <valentin.zagrebin.93@gmail.com>
2022-12-19 15:52:27 +0300

    styled calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -22,10 +22,10 @@ const Calendar: FC = () => {
 
   return (
     <div className={classes.calendar}>
-        <div>
-          <button onClick={() => dispatch(calendarSlice.actions.decrementMonth())}>{'<'}</button>
-          {monthName} {year}
-          <button onClick={() => dispatch(calendarSlice.actions.incrementMonth())}>{'>'}</button>
+        <div className={classes.months}>
+          <button className={classes.months__button} onClick={() => dispatch(calendarSlice.actions.decrementMonth())}>{'<'}</button>
+          <p>{`${monthName} ${year}г.`}</p>
+          <button className={classes.months__button} onClick={() => dispatch(calendarSlice.actions.incrementMonth())}>{'>'}</button>
         </div>
         <div>
           <ul className={classes.daysOfWeek__list}>
@@ -34,14 +34,20 @@ const Calendar: FC = () => {
           </ul>
         </div>
         <div>
-          <ul className={classes.dayOfMonth__list}>
+          <ul className={classes.daysOfMonth__list}>
             {days.map((day) =>
             <li
               className={classes.daysOfMonth__item}
               key={day.dayOfMonth}
               style={ day.dayOfMonth === 1 ? {gridColumn: day.dayOfWeek} : {}}
             >
-              {day.dayOfMonth}
+              <input
+                className={classes.daysOfMonth__radio}
+                type="radio"
+                name="dayOfMonth"
+                id={String(day.dayOfMonth)}
+              />
+              <label className={classes.daysOfMonth__label} htmlFor={String(day.dayOfMonth)}>{day.dayOfMonth}</label>
             </li>)}
           </ul>
         </div>
